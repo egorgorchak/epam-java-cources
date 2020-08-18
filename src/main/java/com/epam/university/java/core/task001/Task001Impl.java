754cacae899e5b51aeca1e281a886e85735443bb
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-08-18 23:50:48 +0300

    Task001 execution by Egor Laptev

---
+++ src/main/java/com/epam/university/java/core/task001/Task001Impl.java
@@ -0,0 +1,47 @@
+package com.epam.university.java.core.task001;
+
+/*
+* Completed by Laptev Egor 18.02.2020
+* */
+
+public class Task001Impl implements Task001 {
+    @Override
+    public double addition(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+        double first = Double.parseDouble(firstNumber);
+        double second = Double.parseDouble(secondNumber);
+        return first + second;
+    }
+
+    @Override
+    public double subtraction(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+        double first = Double.parseDouble(firstNumber);
+        double second = Double.parseDouble(secondNumber);
+        return first - second;
+    }
+
+    @Override
+    public double multiplication(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+        double first = Double.parseDouble(firstNumber);
+        double second = Double.parseDouble(secondNumber);
+        return first * second;
+    }
+
+    @Override
+    public double division(String firstNumber, String secondNumber) {
+        if (firstNumber == null || secondNumber == null) {
+            throw new IllegalArgumentException();
+        }
+        double first = Double.parseDouble(firstNumber);
+        double second = Double.parseDouble(secondNumber);
+        return first / second;
+    }
+}
