b1d7bf8469103e55d6d29e675603f36647ba9833
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-08-28 00:11:01 +0300

    Task008 execution

---
+++ src/main/java/com/epam/university/java/core/task008/Task008Impl.java
@@ -0,0 +1,30 @@
+package com.epam.university.java.core.task008;
+
+/*
+ * Completed by Laptev Egor 27.08.2020
+ * */
+
+public class Task008Impl implements Task008 {
+
+    @Override
+    public boolean isValid(String sourceString) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < sourceString.length(); i++) {
+            if (isBracket(sourceString.charAt(i))) {
+                stringBuilder.append(sourceString.charAt(i));
+            }
+        }
+        String newSource = stringBuilder.toString();
+
+        while (newSource.contains("()") || newSource.contains("[]") || newSource.contains("{}")) {
+            newSource = newSource.replace("()", "");
+            newSource = newSource.replace("[]", "");
+            newSource = newSource.replace("{}", "");
+        }
+        return newSource.length() == 0;
+    }
+
+    private static boolean isBracket(char ch) {
+        return ch == '(' || ch == ')' || ch == '[' || ch == ']' || ch == '{' || ch == '}';
+    }
+}
