4ffcd36ef72c9e585ec69e2658ad7c6f7f7f8d26
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-08-30 19:17:57 +0300

    Task010 execution

---
+++ src/main/java/com/epam/university/java/core/task010/Task010Impl.java
@@ -0,0 +1,46 @@
+package com.epam.university.java.core.task010;
+
+/*
+ * Completed by Laptev Egor 30.08.2020
+ * */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task010Impl implements Task010 {
+    @Override
+    public Map<String, Integer> countWordNumbers(File source) {
+        if (source == null) {
+            throw new IllegalArgumentException();
+        }
+
+        ArrayList<String> words = new ArrayList<>();
+        try (BufferedReader reader =
+                     new BufferedReader(new InputStreamReader(new FileInputStream(source)))) {
+            String nextLine = reader.readLine().toLowerCase();
+            words.addAll(Arrays.asList(nextLine.split("[ .,!?:]")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        words.removeIf(string -> string.isEmpty());
+
+        Map<String, Integer> result = new HashMap<>();
+        for (String string1 : words) {
+            int freq = 0;
+            for (String string2 : words) {
+                if (string1.equals(string2)) {
+                    freq = ++freq;
+                }
+            }
+            result.put(string1, freq);
+        }
+
+        return result;
+    }
+}
