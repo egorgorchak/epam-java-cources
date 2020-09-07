17ceba8fae3c00ab5bf5ade92e19635ca7461417
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-09-08 00:34:54 +0300

    Figures and Vertexes implementation with files =)

--- src/main/java/com/epam/university/java/core/task013/FigureImpl.java
+++ src/main/java/com/epam/university/java/core/task013/FigureImpl.java
@@ -1,2 +1,18 @@
-package com.epam.university.java.core.task013;public class FigureImpl {
+package com.epam.university.java.core.task013;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class FigureImpl implements Figure {
+    ArrayList<Vertex> vertexes = new ArrayList<>();
+
+    @Override
+    public void addVertex(Vertex vertex) {
+        vertexes.add(vertex);
+    }
+
+    @Override
+    public Collection<Vertex> getVertexes() {
+        return vertexes;
+    }
 }
