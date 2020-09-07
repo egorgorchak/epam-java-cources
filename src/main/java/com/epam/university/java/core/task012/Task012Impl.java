6ba487225dd5a45080e84e546f07d1416b3091fc
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-09-07 23:42:52 +0300

    Task012 execution

--- src/main/java/com/epam/university/java/core/task012/Task012Impl.java
+++ src/main/java/com/epam/university/java/core/task012/Task012Impl.java
@@ -1,18 +1,47 @@
 package com.epam.university.java.core.task012;
 
+/*
+ * Completed by Laptev Egor 07.09.2020
+ * */
+
 import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.Stack;
 
 public class Task012Impl implements Task012 {
     @Override
     public Graph invokeActions(Graph sourceGraph, Collection<GraphAction> actions) {
-        for (GraphAction action : actions) {
-            act -> act.;
+        if (sourceGraph == null || actions == null || actions.isEmpty()) {
+            throw new IllegalArgumentException();
         }
-        return null;
+        for (GraphAction action : actions) {
+            action.run(sourceGraph);
+        }
+        return sourceGraph;
     }
 
     @Override
     public boolean pathExists(Graph graph, int from, int to) {
-        return false;
+        if (graph == null) {
+            throw new IllegalArgumentException();
+        }
+        int sizeOfList = ((GraphImpl) graph).getGraphMap().size();
+        if (from > sizeOfList || to > sizeOfList) {
+            throw new IllegalArgumentException();
+        }
+        Set<Integer> visited = new LinkedHashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(from);
+        while (!stack.isEmpty()) {
+            int vertex = stack.pop();
+            if (!visited.contains(vertex)) {
+                visited.add(vertex);
+                for (Integer vert : graph.getAdjacent(vertex)) {
+                    stack.push(vert);
+                }
+            }
+        }
+        return visited.contains(to);
     }
 }
