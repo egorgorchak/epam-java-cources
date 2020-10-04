fa70d551042834a14fa64f7ea2282969f7547bbf
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-10-04 23:12:13 +0300

    task022 execution and almost done task021

--- src/main/java/com/epam/university/java/core/task022/Task022Impl.java
+++ src/main/java/com/epam/university/java/core/task022/Task022Impl.java
@@ -1,15 +1,55 @@
 package com.epam.university.java.core.task022;
 
+import java.util.ArrayList;
 import java.util.Collection;
+
+/*
+ * Completed by Laptev Egor 04.10.2020
+ * */
 
 public class Task022Impl implements Task022 {
     @Override
     public int maxSum(Collection<Integer> numbers) {
-        return 0;
+        if (numbers == null || numbers.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        ArrayList<Integer> numbersList = new ArrayList<>(numbers);
+        int maxSum = 0;
+        for (int i = 0; i < numbersList.size(); i++) {
+            int currentMaxSum = 0;
+            for (int j = 0; j < numbersList.size(); j++) {
+                if (i == j) {
+                    continue;
+                }
+                currentMaxSum += numbersList.get(j);
+            }
+            if (i == 0) {
+                maxSum = currentMaxSum;
+            } else if (maxSum < currentMaxSum) {
+                maxSum = currentMaxSum;
+            }
+        }
+        return maxSum;
     }
-//sdfsdgadsgdg
+
     @Override
     public int minSum(Collection<Integer> numbers) {
-        return 0;
+        ArrayList<Integer> numbersList = new ArrayList<>(numbers);
+        int minSum = 0;
+        for (int i = 0; i < numbersList.size(); i++) {
+            int currentMinSum = 0;
+            for (int j = 0; j < numbersList.size(); j++) {
+                if (i == j) {
+                    continue;
+                }
+                currentMinSum += numbersList.get(j);
+            }
+            if (i == 0) {
+                minSum = currentMinSum;
+            } else if (minSum > currentMinSum) {
+                minSum = currentMinSum;
+            }
+        }
+        return minSum;
     }
 }
