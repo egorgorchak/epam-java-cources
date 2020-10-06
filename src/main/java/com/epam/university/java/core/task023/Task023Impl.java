1eb52bf0bf52e734dcf5829e94c9eaf2678329bc
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-10-06 12:10:52 +0300

    Task023 execution 2

--- src/main/java/com/epam/university/java/core/task023/Task023Impl.java
+++ src/main/java/com/epam/university/java/core/task023/Task023Impl.java
@@ -3,5 +3,22 @@ package com.epam.university.java.core.task023;
  * Completed by Laptev Egor 06.10.2020
  * */
 
-public class Task023Impl {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task023Impl implements Task023 {
+    @Override
+    public String extract(String phoneString) {
+        if (phoneString == null || phoneString.length() < 11) {
+            throw new IllegalArgumentException();
+        }
+        Pattern p = Pattern
+                .compile("^[+][7][-(]|^[8][(]|^[+][7][ ][(]|^[+][7][ ]|[+][7]|^[8][-]|^[8]");
+        Matcher m = p.matcher(phoneString);
+        int offset = 0;
+        while (m.find()) {
+            offset += m.end();
+        }
+        return phoneString.substring(offset, offset + 3);
+    }
 }
