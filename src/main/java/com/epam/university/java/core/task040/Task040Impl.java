0fab5ac9f9932c50bc9094e3ac5387cf35cce1b5
Laptev Egor (laptop) <egor.gorchak@gmail.com>
2020-10-08 18:48:30 +0300

    task040 update

--- src/main/java/com/epam/university/java/core/task040/Task040Impl.java
+++ src/main/java/com/epam/university/java/core/task040/Task040Impl.java
@@ -3,54 +3,43 @@ package com.epam.university.java.core.task040;
  * Completed by Laptev Egor 08.10.2020
  * */
 
+import javax.print.attribute.IntegerSyntax;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Task040Impl implements Task040 {
-    boolean isStrike = false;
-    boolean isSpare = false;
-
     @Override
     public int countScore(String str) {
         if (str == null || str.isEmpty()) {
             throw new IllegalArgumentException();
         }
-        String[] frames = str.split(" ");
-
+        char[] shoots = str.toCharArray();
+        ArrayList<Character> characters = new ArrayList<>();
+        for (char shoot : shoots) {
+            if (shoot != ' ') {
+                characters.add(shoot);
+            }
+        }
         int score = 0;
         int frameScore = 0;
+        for (int i = 0; i < characters.size(); i++) {
+            if (characters.get(i) == 'X') {
+                int current = 10;
+                int next1int;
+                int next2int;
+                char next1 = characters.get(i + 1);
+                char next2 = characters.get(i + 2);
+                if (next1 == 'X') {
+                    next1int = 10;
+                } else if (next2 == 'X') {
+                    next2int = 10;
+                }
 
-        for (int i = 0; i < frames.length; i++) {
-            String currentFrame = frames[i];
-            int[] shoots = countShoot(currentFrame);
-            if (isStrike) {
-                frameScore = frameScore + shoots[0];
-                countShoot(frames[i + 1]);
+
 
             }
-            score = score + frameScore;
         }
-
-        return score;
-    }
-
-    public List<Integer> countShoot(String shoot) {
-        ArrayList<Integer> shoots = new ArrayList<>();
-        int firstThrow = 0;
-        int secondThrow = 0;
-        if (shoot.length() == 1) {
-            firstThrow = 10;
-            isStrike = true;
-            return new int[] {firstThrow};
-        } else if (shoot.contains("/")) {
-            firstThrow = Integer.parseInt(String.valueOf(shoot.charAt(0)));
-            secondThrow = 10 - firstThrow;
-            isSpare = true;
-            return new int[] {firstThrow, secondThrow};
-        } else {
-            firstThrow = Integer.parseInt(String.valueOf(shoot.charAt(0)));
-            secondThrow = Integer.parseInt(String.valueOf(shoot.charAt(1)));
-            return new int[] {firstThrow, secondThrow};
-        }
+        return 0;
     }
 }
