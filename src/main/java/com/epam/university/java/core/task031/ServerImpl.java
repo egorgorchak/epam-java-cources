3f89deb01c1c77872fd9e7e378e0736f099b7a2f
egorgorchak <36458220+egorgorchak@users.noreply.github.com>
2020-11-12 14:13:06 +0300

    Task031 fix

--- src/main/java/com/epam/university/java/core/task031/ServerImpl.java
+++ src/main/java/com/epam/university/java/core/task031/ServerImpl.java
@@ -1,6 +1,6 @@
 package com.epam.university.java.core.task031;
 /*
- * Completed by Laptev Egor 11.10.2020
+ * Created by Laptev Egor 12.11.2020
  * */
 
 import java.io.BufferedReader;
@@ -8,78 +8,55 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayDeque;
+import java.util.LinkedList;
 
 public class ServerImpl implements Server {
-    private static volatile ArrayDeque<String> messages;
     private ServerSocket serverSocket;
-    private boolean isServerRunning;
-    private boolean isClientRunning;
+    private LinkedList<String> messages = new LinkedList<>();
+    private boolean isRun = true;
 
-    public ServerImpl() {
-        messages = new ArrayDeque<>();
+    @Override
+    public String readMessage() {
+        if (messages.isEmpty()) {
+            return "";
+        }
+        return messages.pollLast();
     }
 
     @Override
     public void start() {
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    serverSocket = new ServerSocket(9999);
-                    isServerRunning = true;
-                    while (isServerRunning) {
-                        Socket socket = serverSocket.accept();
-                        Thread thread = new Thread(new Runnable() {
-                            @Override
-                            public void run() {
-                                isClientRunning = true;
-                                try (BufferedReader input = new BufferedReader(
-                                        new InputStreamReader(socket.getInputStream()))) {
-                                    while (isClientRunning) {
-                                        if (input.ready()) {
-                                            String str = input.readLine();
-                                            messages.push(str);
-                                        }
-                                    }
-                                } catch (IOException e) {
-                                    e.printStackTrace();
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(9999);
+                while (!serverSocket.isClosed()) {
+                    Socket clientSocket = serverSocket.accept();
+                    new Thread(() -> {
+                        try (BufferedReader in = new BufferedReader(
+                                new InputStreamReader(clientSocket.getInputStream())
+                        )) {
+                            while (isRun) {
+                                if (in.ready()) {
+                                    messages.addLast(in.readLine());
                                 }
                             }
-                        });
-                        thread.start();
-                    }
-                } catch (IOException e) {
-                    System.out.println("Socket is closed");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
-        thread1.start();
+        }).start();
     }
 
     @Override
     public void stop() {
-        isServerRunning = false;
-        isClientRunning = false;
+        isRun = false;
         try {
-            if (serverSocket != null) {
-                serverSocket.close();
-            }
+            serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public String readMessage() {
-        try {
-            Thread.sleep(250);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        if (messages.size() == 0) {
-            return "";
-        }
-        return messages.poll();
     }
 }
